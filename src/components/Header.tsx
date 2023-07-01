d97a094966ecd73008d7684855c0c7896638ae9d
ClaytonDewey <clay@dryadmedia.com>
2023-07-01 09:35:47 -0500

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type HeaderProps = {};
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <header className='header'>
+      <h1>todo</h1>
+    </header>
+  );
+};
+export default Header;
