a672ead0092ba078b2e10620d7481e2434e43542
ClaytonDewey <clay@dryadmedia.com>
2023-07-01 09:38:13 -0500

    Add class for styling

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 type TodoProps = {};
 
 const Todo: React.FC<TodoProps> = () => {
-  return <div>Todo today...</div>;
+  return <div className='todo'>Todo today...</div>;
 };
 export default Todo;
