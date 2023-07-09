76fc9d2c3968b26bfbec75587f3994380f54552f
Clayton Dewey <clay@dryadmedia.com>
2023-07-09 08:34:33 -0500

    Reformat for styled components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, styled } from 'styled-components';
 import TogglerButton from './TogglerButton';
 import GlobalStyle from '../styles/global';
 import ThemeContext from '../contexts/ThemeContext';
@@ -9,6 +9,17 @@ import Header from './Header';
 import TodoFooter from './TodoFooter';
 import TodoList from './TodoList';
 
+const StyledContainer = styled.div`
+  width: 100%;
+  max-width: 54rem;
+  margin: 4.8rem auto 2rem;
+  padding: 0 2.3rem;
+
+  @media screen and (min-width: 55.125em) {
+    margin: 8rem auto 2rem;
+  }
+`
+
 function App() {
   const { theme, themeToggler } = useThemeMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
@@ -17,14 +28,14 @@ function App() {
     <ThemeContext>
       <ThemeProvider theme={themeMode}>
         <GlobalStyle />
-        <div className='container'>
+        <StyledContainer>
           <Header>
             <TogglerButton themeToggler={themeToggler} />
           </Header>
           <AddTodo />
           <TodoList />
           <TodoFooter />
-        </div>
+        </StyledContainer>
       </ThemeProvider>
     </ThemeContext>
   );
