febbea82227ba12ac682dd099a7888600a1271a0
Clayton Dewey <clay@dryadmedia.com>
2023-07-08 20:30:46 -0500

    Turn header into styled component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type HeaderProps = {
   children: JSX.Element
 };
 
+const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 3.2rem;
+
+  h1 {
+    color: var(--dark-text);
+  }
+`;
+
 const Header: React.FC<HeaderProps> = ({ children }) => {
   return (
-    <header className='header'>
+    <HeaderStyled>
       <h1>todo</h1>
       {children}
-    </header>
+    </HeaderStyled>
   );
 };
 export default Header;
