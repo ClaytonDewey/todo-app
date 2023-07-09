a831ea9acb91e6ddde9b1e1952bebd4303b2ea51
Clayton Dewey <clay@dryadmedia.com>
2023-07-09 08:34:17 -0500

    Move base & typography components to global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,14 +34,96 @@ const globalStyle = createGlobalStyle`
 
   }
 
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    outline: 0;
-  }
+  /* Box sizing rules */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
 
-  body  {
+/* Remove default margin */
+body, h1, h2, h3, h4, p, figure, blockquote, dl, dd {
+  margin: 0;
+}
+
+/* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+ul[role='list'],
+ol[role='list'] {
+  list-style: none;
+}
+
+/* Set core root defaults */
+html:focus-within {
+  scroll-behavior: smooth;
+}
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+}
+
+/* A elements that don't have a class get default styles */
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+/* Make images easier to work with */
+img,
+picture {
+  max-width: 100%;
+  display: block;
+}
+
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+@media (prefers-reduced-motion: reduce) {
+  html:focus-within {
+   scroll-behavior: auto;
+  }
+  
+  *,
+  *::before,
+  *::after {
+    animation-duration: 0.01ms !important;
+    animation-iteration-count: 1 !important;
+    transition-duration: 0.01ms !important;
+    scroll-behavior: auto !important;
+  }
+}
+
+  *,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+html {
+  // This defines what 1rem is.
+  font-size: 62.5%; // 1rem = 10px;
+  font-style: normal;
+  font-weight: normal;
+}
+
+html,
+body {
+  margin: 0px auto;
+  width: 100%;
+  box-sizing: border-box;
+}  
+
+body  {
+  position: relative;
     background-repeat: no-repeat;
     background-size: contain;
     background-position: top center;
@@ -52,14 +134,24 @@ const globalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: ${({ theme }: GlobalThemeProps) => theme.background};
-    background-image: url(${({ theme }: GlobalThemeProps) => theme.backgroundImage});
+    background-image: url(${({ theme }: GlobalThemeProps) => theme.backgroundImageSm});
+
+    @media screen and (min-width: 55.125em) {
+      background-image: url(${({ theme }: GlobalThemeProps) => theme.backgroundImage});
+    }
   }
 
   h1 {
-    font-size: 3.375rem;
-    color: ${({ theme }: GlobalThemeProps) => theme.text};
-  }
+  text-transform: uppercase;
+  font-size: 2.7rem;
+  font-weight: 700;
+  letter-spacing: 9.5px;
 
+  @media screen and (min-width: 55.125em) {
+    font-size: 4rem;
+    letter-spacing: 6px;
+  }
+}
 `;
 
 export default withTheme(globalStyle);
