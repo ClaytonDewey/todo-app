10072685e8ccc2de819334c1a4411b7a1b77afd5
Clayton Dewey <clay@dryadmedia.com>
2023-07-09 08:33:05 -0500

    Add bg image for small devices

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,20 +1,25 @@
 import darkImg from '../images/bg-desktop-dark.jpg';
+import darkImgSm from '../images/bg-mobile-dark.jpg';
 import lightImg from '../images/bg-desktop-light.jpg';
+import lightImgSm from '../images/bg-mobile-light.jpg';
 
 export interface ThemeProps {
   backgroundImage: string;
+  backgroundImageSm: string;
   background: string;
   text: string;
 }
 
 export const darkTheme: ThemeProps = {
   backgroundImage: darkImg,
+  backgroundImageSm: darkImgSm,
   background: 'var(--dark-background)',
   text: 'var(--dark-text)',
 };
 
 export const lightTheme: ThemeProps = {
   backgroundImage: lightImg,
+  backgroundImageSm: lightImgSm,
   background: 'var(--light-background)',
   text: 'var(--light-text)',
 };
