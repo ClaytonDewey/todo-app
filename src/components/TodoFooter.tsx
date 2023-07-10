5b8be7f3aa560ecf02fd829214e6824a9eb9696d
ClaytonDewey <clay@dryadmedia.com>
2023-07-10 08:45:11 -0500

    Update todo footer to styled component

--- src/components/TodoFooter.tsx
+++ src/components/TodoFooter.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Button from './Button';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+    &.footer-todo {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: hsl(235, 24%, 19%);
+    height: 4.8rem;
+    border-radius: var(--radius);
+    margin-top: 1.6rem;
+  }
+
+`
 
 type TodoFooterProps = {};
 
 const TodoFooter: React.FC<TodoFooterProps> = () => {
   return (
-    <footer className='footer-todo'>
+    <StyledFooter className='footer-todo'>
       <Button className='btn btn-text active'>All</Button>
       <Button className='btn btn-text'>Active</Button>
       <Button className='btn btn-text'>Completed</Button>
-    </footer>
+    </StyledFooter>
   );
 };
 export default TodoFooter;
