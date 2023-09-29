6cff7fe1778ec6c267ec98d9d0e117180a0d2219
ClaytonDewey <clay@dryadmedia.com>
2023-09-29 11:08:10 -0500

    Show "nothing to do" text if no todo items exist

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { TodoItem } from './TodoItem';
 import { useTodo } from '../context';
 
@@ -7,21 +6,18 @@ export const TodoList = () => {
 
   if (!todos.length) {
     return (
-      <div className="todo__list">
-        <div className="todo todo__empty">
-          <h2>
-            You have nothing to do!
-          </h2>
+      <div className='todo__list'>
+        <div className='todo todo__empty'>
+          <h2>You have nothing to do!</h2>
         </div>
       </div>
-    )
+    );
   }
   return (
     <div className='todo__list'>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todos.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
