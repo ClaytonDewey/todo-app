246168824c2609292fb92414b3c619331635040a
ClaytonDewey <clay@dryadmedia.com>
2023-09-29 10:10:31 -0500

    Import useTodo, Input, and implement them

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useTodo } from '../context';
+import { Input } from './Input';
 
-type AddTodoProps = {};
+const AddTodo = () => {
+  const [input, setInput] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { addTodo } = useTodo();
 
-const AddTodo: React.FC<AddTodoProps> = () => {
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [])
+
+  const handleSubmission = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input.trim() !== '') {
+      addTodo(input);
+      setInput('');
+      toast.success('Todo added successfylly!');
+    } else {
+      toast.error('Todo field cannot be empty!')
+    }
+  }
+
   return (
-    <div className='todo todo__add'>
-      <label htmlFor='todo' className='sr-only'>
-        New Todo
-      </label>
-      <input
-        type='text'
-        id='todo'
-        name='todo'
-        placeholder='Create a new todo...'
-      />
-    </div>
-  );
-};
+    <form onSubmit={handleSubmission}>
+      <div className='todo todo__add'>
+        <label htmlFor='todo' className='sr-only'>
+          New Todo
+        </label>
+        <Input
+          ref={inputRef}
+          type="text"
+          id='todo'
+          name='todo'
+          placeholder='Create a new todo...'
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        {/* <button>
+          Submit
+        </button> */}
+      </div>
+    </form>
+  )
+}
+
 export default AddTodo;
