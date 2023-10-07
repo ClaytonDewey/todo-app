01d10dca322bc56e22ed1d7ec6e23ac90dd142f6
Clayton Dewey <clay@dryadmedia.com>
2023-10-07 12:57:38 -0500

    Export page footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { AddTodo } from './AddTodo';
+export { Footer } from './Footer';
 export { Input } from './Input';
 export { TodoItem } from './TodoItem';
 export { TodoList } from './TodoList';
